a1c1c1e32ca1f33a2a256ff20d9e297926acf811
ellasutra <101535816+ellasutra@users.noreply.github.com>
2022-03-14 22:47:05 +0700

    Add files via upload

---
+++ function (1).php
@@ -0,0 +1,13 @@
+<?php
+$db = mysqli_connect("localhost", "root", "", "ella");
+
+function query($query)
+{
+    global $db;
+    $result = mysqli_query($db, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
---
+++ index (1).php
@@ -0,0 +1,65 @@
+<?php
+// Koneksi ke database
+require 'function.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Data Mahasiswa</title>
+</head>
+
+<body>
+
+    <h1 align=center>Daftar Mahasiswa</h1>
+
+    <section>
+    <center><button type="button" class="btn btn-primary">
+    <a href="tambah.php">tambah data</a>
+    </button></center>
+</section>
+    <br><br>
+
+    <table align="center" border="1" cellpadding="10" cellspacing="0">
+        <tr style="background-color: blue">
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Photo</th>
+            <th>Nama</th>
+            <th>NPM</th>
+            <th>Jurusan</th>
+            <th>Email</th>
+        </tr>
+
+        <?php $i = 1; ?>
+        <?php foreach ($mahasiswa as $row) : ?>
+            <tr>
+
+                <td> <?= $i; ?> </td>
+
+                <td>
+                    <a href="">Ubah</a> |
+                    <a href="">Hapus</a>
+                </td>
+                <td><img src="img/<?php echo $row["gambar"] ?>" width="50"></td>
+                <td><?php echo $row["nama"] ?></td>
+                <td><?php echo $row["npm"] ?></td>
+                <td><?php echo $row["jurusan"] ?></td>
+                <td><?php echo $row["email"] ?></td>
+            </tr>
+            <?php $i++; ?>
+        <?php endforeach; ?>
+
+    </table>
+
+</body>
+
+</html>
